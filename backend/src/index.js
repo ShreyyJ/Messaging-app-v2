2e968611fa40020da7acd6241c1ae42d87209678
Shreya <shreyareddy5544@gmail.com>
2026-01-29 00:51:12 +0530

    fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,6 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-import path from "path";
-import { fileURLToPath } from "url";
-
 import { connectDB } from "../lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
@@ -15,8 +12,6 @@ import { app, server } from "../lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -31,14 +26,6 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
-
-  app.use((req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
-  });
-}
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
